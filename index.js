8cc381d0445ca4d3adcac8911bdc35a8b385bac7
buranch <birukmisanaw@gmail.com>
2018-04-25 15:05:50 +0300

    fixing

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var dbFeeder = require('./categories/dbFeeder');
 //==============================================================DB connection++++++++++++++++++++++++++++++++++++++++++++++++++=
 // DB_CONN = 'mongodb://mbelachew:root@ds119700-a0.mlab.com:19700,ds119700-a1.mlab.com:19700/rodmisc?replicaSet=rs-ds119700';
 // DB_CONN = 'mongodb://buranch:mLab130879@ds153869.mlab.com:53869/walmert'
-// DB_CONN = 'mongodb://mbelachew:root@ds119700-a0.mlab.com:19700,ds119700-a1.mlab.com:19700/rodmisc?replicaSet=rs-ds119700'
+DB_CONN = 'mongodb://mbelachew:root@ds119700-a0.mlab.com:19700,ds119700-a1.mlab.com:19700/rodmisc?replicaSet=rs-ds119700'
 // DB_CONN = 'mongodb://buranch:mLab130879@ds153869.mlab.com:53869/walmert'
-DB_CONN = 'mongodb://localhost:27017/walmart'
+// DB_CONN = 'mongodb://localhost:27017/walmart'
 mongoose.connect(DB_CONN, (err, result) => {
     if (err) {
         console.log("Error while connecting to the DB");
